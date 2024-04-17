99c3fa007dec47a04a2ac681a7a6f1558697e3de
rafael_nunes <rafa.nuns17@gmail.com>
2024-04-17 17:53:28 -0300

    Função de validação na entrada de dados de cliente ao salvar

--- JavaFX-MVC-Incompleto/src/javafxmvc/controller/FXMLAnchorPaneCadastrosClientesDialogController.java
+++ JavaFX-MVC-Incompleto/src/javafxmvc/controller/FXMLAnchorPaneCadastrosClientesDialogController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -39,7 +40,7 @@ public class FXMLAnchorPaneCadastrosClientesDialogController implements Initiali
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
     }
 
     public Stage getDialogStage() {
@@ -68,20 +69,51 @@ public class FXMLAnchorPaneCadastrosClientesDialogController implements Initiali
         this.textFieldClienteCPF.setText(cliente.getCpf());
         this.textFieldClienteTelefone.setText(cliente.getTelefone());
     }
-    
+
     @FXML
     public void handleButtonConfirmar() {
-        cliente.setNome(textFieldClienteNome.getText());
-        cliente.setCpf(textFieldClienteCPF.getText());
-        cliente.setTelefone(textFieldClienteTelefone.getText());
-        
-        buttonConfirmarClicked = true;
-        
-        dialogStage.close();
+        if (validarEntradaDeDadosCliente()) {
+            
+            cliente.setNome(textFieldClienteNome.getText());
+            cliente.setCpf(textFieldClienteCPF.getText());
+            cliente.setTelefone(textFieldClienteTelefone.getText());
+
+            buttonConfirmarClicked = true;
+
+            dialogStage.close();
+        }
     }
-    
+
     @FXML
     public void handleButtonCancelar() {
         dialogStage.close();
+    }
+
+    private boolean validarEntradaDeDadosCliente() {
+        String errorMessage = "";
+        String nome = textFieldClienteNome.getText();
+        String cpf = textFieldClienteCPF.getText();
+        String telefone = textFieldClienteTelefone.getText();
+
+        if (nome == null || nome.length() == 0) {
+            errorMessage += "Digite o nome do cliente!\n";
+        }
+        if (cpf == null || cpf.length() == 0) {
+            errorMessage += "Digite o CPF do cliente!\n";
+        }
+        if (telefone == null || telefone.length() == 0) {
+            errorMessage += "Digite um Telefone para contato!\n";
+        }
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erro no cadastro");
+            alert.setContentText("Campos invalidos, por favor, corrija-os...");
+            alert.setContentText(errorMessage);
+            alert.show();
+            return false;
+        }
     }
 }
