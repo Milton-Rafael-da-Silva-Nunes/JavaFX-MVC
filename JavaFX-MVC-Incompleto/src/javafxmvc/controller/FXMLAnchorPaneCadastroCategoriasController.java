8603bf1e3fa3837d0bdd64dc1f198b033884bf31
rafael_nunes <rafa.nuns17@gmail.com>
2024-04-19 11:23:37 -0300

    Iniciando CRUD de Categoria

--- JavaFX-MVC-Incompleto/src/javafxmvc/controller/FXMLAnchorPaneCadastroCategoriasController.java
+++ JavaFX-MVC-Incompleto/src/javafxmvc/controller/FXMLAnchorPaneCadastroCategoriasController.java
@@ -8,9 +8,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
 import javafxmvc.model.dao.CategoriaDAO;
 import javafxmvc.model.database.Database;
 import javafxmvc.model.database.DatabaseFactory;
@@ -21,17 +26,31 @@ import javafxmvc.model.domain.Categoria;
  * @author Rafael Nunes
  */
 public class FXMLAnchorPaneCadastroCategoriasController implements Initializable {
-    
+
     @FXML
     private TableView<Categoria> tableViewCategorias;
     @FXML
     private TableColumn<Categoria, String> tableColumnCategoriaCodigo;
     @FXML
     private TableColumn<Categoria, String> tableColumnCategoriaNome;
-    
+    @FXML
+    private GridPane gridPaneCategoria;
+    @FXML
+    private Label labelCategoriaCodigo;
+    @FXML
+    private TextField textFielCategoriaNome;
+    @FXML
+    private Button buttonInserir;
+    @FXML
+    private Button buttonAlterar;
+    @FXML
+    private Button buttonRemover;
+    @FXML
+    private Button gripPaneCategoriaButtonSalvar;
+
     private List<Categoria> listCategorias;
     private ObservableList<Categoria> observableListCategorias;
-    
+
     private final Database database = DatabaseFactory.getDatabase("postgresql");
     private final Connection connection = database.conectar();
     private final CategoriaDAO categoriaDAO = new CategoriaDAO();
@@ -40,15 +59,71 @@ public class FXMLAnchorPaneCadastroCategoriasController implements Initializable
     public void initialize(URL url, ResourceBundle rb) {
         categoriaDAO.setConnection(connection);
         carregarTableViewCategorias();
-    }    
-    
+        gridPaneCategoria.setVisible(false);
+    }
+
     public void carregarTableViewCategorias() {
         tableColumnCategoriaCodigo.setCellValueFactory(new PropertyValueFactory<>("cdCategoria"));
         tableColumnCategoriaNome.setCellValueFactory(new PropertyValueFactory<>("descricao"));
-        
+
         listCategorias = categoriaDAO.listar();
-        
+
         observableListCategorias = FXCollections.observableArrayList(listCategorias);
         tableViewCategorias.setItems(observableListCategorias);
-    } 
+    }
+
+    @FXML
+    public void hundleButtonInserir() {
+        gridPaneCategoria.setVisible(true);
+    }
+
+    @FXML
+    public void hundleButtonAlerar() {
+        gridPaneCategoria.setVisible(true);
+        Categoria categoria = tableViewCategorias.getSelectionModel().getSelectedItem();
+        if(categoria != null) {
+            categoriaDAO.alterar(categoria);
+            carregarTableViewCategorias();
+        }
+    }
+
+    @FXML
+    public void hundleButtonRemover() {
+
+    }
+
+    @FXML
+    public void hundleGridPaneButtonSalvar() {
+        Categoria categoria = new Categoria(); 
+        categoria.setDescricao(textFielCategoriaNome.getText());
+        salvarDadosNovaCategoria(categoria);
+        gridPaneCategoria.setVisible(false);
+    }
+
+    private void salvarDadosNovaCategoria(Categoria categoria) {
+        if (validarDadosNovaCategoria()) {
+            categoriaDAO.inserir(categoria);
+            carregarTableViewCategorias();
+        }
+    }
+
+    private boolean validarDadosNovaCategoria() {
+        String errorMessage = "";
+        String nomeNovaCategoria = textFielCategoriaNome.getText();
+
+        if (nomeNovaCategoria == null || nomeNovaCategoria.length() == 0) {
+            errorMessage += "Digite o nome da Categoria!\n";
+        }
+
+        if (errorMessage.isEmpty()) {
+            return true;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erro no cadastro");
+            alert.setContentText("Campos invalidos, por favor, corrija-os...");
+            alert.setContentText(errorMessage);
+            alert.show();
+            return false;
+        }
+    }
 }
